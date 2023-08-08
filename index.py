7857cd3291d415dcfad120967aebcd9bf096a085
Yeminfu <yeminfu4319@gmail.com>
2023-08-09 00:57:31 +1000

    add relation

--- index.py
+++ index.py
@@ -9,9 +9,9 @@ vk_session = vk_api.VkApi(token=token)
 
 vk = vk_session.get_api()
 # 153 /хабаровск
-response = vk.users.search(city=1, age_from=25, age_to=40, count=1000, sex=1, offset=0, fields='photo_200')
+response = vk.users.search(city=1, age_from=25, age_to=40, count=1000, sex=1, offset=100, fields='photo_200,relation')
 
 for user in response['items']:
     photo_url = user['photo_200']
-    print(user['id'], user['is_closed'], photo_url)
-    # print(user['first_name'], user['last_name'], user['id'], user['is_closed'])
+    relation = user.get('relation', None)
+    print(user['first_name'], user['last_name'], user['id'], user['is_closed'], photo_url, relation)
