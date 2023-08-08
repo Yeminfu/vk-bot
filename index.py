0423f9eba463667ea8d71df0ecf43667cb915063
Yeminfu <yeminfu4319@gmail.com>
2023-08-09 00:47:43 +1000

    add photo

--- index.py
+++ index.py
@@ -9,7 +9,9 @@ vk_session = vk_api.VkApi(token=token)
 
 vk = vk_session.get_api()
 # 153 /хабаровск
-response = vk.users.search(city=1, age_from=25, age_to=40, count=1000, sex=1, offset=0)
+response = vk.users.search(city=1, age_from=25, age_to=40, count=1000, sex=1, offset=0, fields='photo_200')
 
 for user in response['items']:
-    print(user['first_name'], user['first_name'], user['last_name'], user['id'], user['is_closed'])
+    photo_url = user['photo_200']
+    print(user['id'], user['is_closed'], photo_url)
+    # print(user['first_name'], user['last_name'], user['id'], user['is_closed'])
