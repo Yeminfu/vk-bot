e9198023078e63ac147fda7f6937c8ff093685cb
Yeminfu <yeminfu4319@gmail.com>
2023-08-09 00:31:21 +1000

    add for

--- index.py
+++ index.py
@@ -8,6 +8,8 @@ token = os.environ.get("TOKEN")
 vk_session = vk_api.VkApi(token=token)
 
 vk = vk_session.get_api()
+# 153 /хабаровск
+response = vk.users.search(city=1, age_from=25, age_to=40, count=1000, sex=1, offset=0)
 
-response = vk.users.search(q='Ольга', city=1, age_from=25, age_to=40)
-print(response)
+for user in response['items']:
+    print(user['first_name'], user['first_name'], user['last_name'], user['id'], user['is_closed'])
